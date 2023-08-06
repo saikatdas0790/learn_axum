9f8f961304e25dd88c973260d485b9cfc5032335
Saikat Das <saikatdas0790@gmail.com>
2023-08-06 09:22:04 +0530

    chore: implement anyhow error response example

--- anyhow-error-response/src/main.rs
+++ anyhow-error-response/src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{http::StatusCode, response, routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(handler));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    println!("Listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> Result<(), AppError> {
+    try_thing()?;
+
+    Ok(())
+}
+
+fn try_thing() -> Result<(), anyhow::Error> {
+    anyhow::bail!("it failed!")
+}
+
+struct AppError(anyhow::Error);
+
+impl response::IntoResponse for AppError {
+    fn into_response(self) -> response::Response {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Something went wrong: {}", self.0),
+        )
+            .into_response()
+    }
+}
+
+impl<E> From<E> for AppError
+where
+    E: Into<anyhow::Error>,
+{
+    fn from(err: E) -> Self {
+        Self(err.into())
+    }
 }
