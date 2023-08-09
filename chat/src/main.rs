6f8feb1dfd2dff44ff1c8abb1e396d9880cba30c
Saikat Das <saikatdas0790@gmail.com>
2023-08-09 15:45:09 +0530

    chore: intermediate save for chat example

---
+++ chat/src/main.rs
@@ -0,0 +1,73 @@
+use std::{
+    collections::HashSet,
+    sync::{Arc, Mutex},
+};
+
+use axum::{
+    extract::{
+        ws::{Message, WebSocket},
+        State, WebSocketUpgrade,
+    },
+    response::{Html, IntoResponse},
+    routing, Router,
+};
+use tokio::sync::broadcast;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "example_chat=trace".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let user_set = Mutex::new(HashSet::new());
+    let (tx, _rx) = broadcast::channel(100);
+
+    let app_state = Arc::new(AppState { user_set, tx });
+
+    let app = Router::new()
+        .route("/", routing::get(index))
+        .route("/websocket", routing::get(websocket_handler))
+        .with_state(app_state);
+}
+
+struct AppState {
+    user_set: Mutex<HashSet<String>>,
+    tx: broadcast::Sender<String>,
+}
+
+async fn index() -> Html<&'static str> {
+    Html(std::include_str!("../chat.html"))
+}
+
+async fn websocket_handler(
+    ws: WebSocketUpgrade,
+    State(state): State<Arc<AppState>>,
+) -> impl IntoResponse {
+    ws.on_upgrade(|socket| websocket(socket, state))
+}
+
+async fn websocket(stream: WebSocket, state: Arc<AppState>) {
+    let (mut sender, mut receiver) = stream.split();
+
+    let mut username = String::new();
+
+    while let Some(Ok(message)) = receiver.next().await {
+        if let Message::Text(name) = message {
+            check_username(&state, &mut username, &name);
+        }
+    }
+}
+
+fn check_username(state: &AppState, string: &mut String, name: &str) {
+    let mut user_set = state.user_set.lock().unwrap();
+
+    if !user_set.contains(name) {
+        user_set.insert(name.to_owned());
+        string.push_str(name);
+    }
+}
